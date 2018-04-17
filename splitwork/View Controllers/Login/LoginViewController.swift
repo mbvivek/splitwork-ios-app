984cc0bc05a2c1a024b09fbe3d81b9709bbe821f
Vivek <mb.vivek1@gmail.com>
2018-04-17 11:26:18 -0400

    Initial commit

---
+++ splitwork/View Controllers/Login/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  splitwork
+//
+//  Created by Vivek Madhusudan Badrinarayan on 4/16/18.
+//  Copyright © 2018 Vivek Madhusudan Badrinarayan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        passwordTextField.isSecureTextEntry = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
