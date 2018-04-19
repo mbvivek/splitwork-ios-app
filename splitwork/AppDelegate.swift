d0270941b29c6072e9b6ee6776bcf2a700a0e161
Swathi Kommaghatta Chandrashekaraiah <kommaghattachandr.s@icd25rr0s6gg7v.local>
2018-04-18 22:29:32 -0400

    HTTP removed

--- splitwork/AppDelegate.swift
+++ splitwork/AppDelegate.swift
@@ -17,15 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        let http:HTTP = HTTP.shared()
-        
-        let completionHandler: (String, [String: Any]) -> () =  {error, data in
-            print("Completion Handler : ")
-            print("Error: \(error)")
-            print("Data: \(data)")
-        }
-        
+   
         //http.get(url: "user_accounts/0", completionHandler: completionHandler)
         
         if let splitViewController = self.window?.rootViewController as? UISplitViewController {
